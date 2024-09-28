8a9144ca467b853cb98630a661b89aea258f69e9
Simon Lecureux <snoprolecureux@gmail.com>
2024-09-28 12:30:16 +0200

    test

--- README.md
+++
@@ -1,5 +0,0 @@
-If you want to be able to build and use server dev, run:
-
-```
-$ npm install
-```
--- src/import.js
+++
@@ -1,8 +0,0 @@
-import Page1 from "./pages/page1";
-import Header from "./components/header.jsx";
-import App from "./App.jsx";
-import Root from "./root.jsx";
-import pfp from "./assets/pfp.jpg";
-import { LanguageContext, LanguageProvider } from "./components/context.jsx";
-
-export { Page1, Header, App, Root, pfp, LanguageContext, LanguageProvider };

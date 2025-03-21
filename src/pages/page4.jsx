b1595c164a78e0d22e9b4f1ca6dcbc92a803f58c
Simon Lecureux <snoprolecureux@gmail.com>
2025-03-21 14:57:03 +0100

    feat(contact page)

--- src/pages/page4.jsx
+++ src/pages/page4.jsx
@@ -1,5 +1,37 @@
+import { useContext } from "react";
+import { LanguageContext } from "../import.js";
+import { Mail, Linkedin } from "lucide-react";
+
+import "./page4.scss";
+
 const Page4 = () => {
-    return <section className="page4" id="contact"></section>;
+    const { language } = useContext(LanguageContext);
+
+    return (
+        <section className="page4" id="contact">
+            <h1 className="contact-title">
+                {language == "fr" ? "Contactez moi" : "Contact me"}
+            </h1>
+            <div className="contact-card">
+                <div className="contact-item">
+                    <Mail className="icon" />
+                    <h3>
+                        {language == "fr" ? "Adresse Email" : "Email Address"}
+                    </h3>
+                    <a href="mailto:sprolecureux@gmail.com">
+                        sprolecureux@gmail.com
+                    </a>
+                </div>
+                <div className="contact-item">
+                    <Linkedin className="icon linkedin" />
+                    <h3>LinkedIn</h3>
+                    <a href="https://www.linkedin.com/in/simonlecureux/">
+                        My page
+                    </a>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Page4;
